03997ca81fa45e97d109d87f8576248daa5299af
Naga Pavankalyan <nagap@techaffinity.com>
2020-11-24 11:20:27 +0530

    react counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React,{ useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>React Counter App using state/hooks</h1>
       </header>
+        <h2>Current value of count is {count}</h2>
+        <button onClick={() => setCount(0)}>Reset Counter</button>
+        <button onClick={() => setCount((count >= 10 ? alert('You have reached the limit') : (count + 1)))}>Increase Counter</button>
+        <button onClick={() => setCount((count == 0 ? alert('counter has been reseted') : (count - 1)))}>Decrease Counter</button>
     </div>
   );
 }
